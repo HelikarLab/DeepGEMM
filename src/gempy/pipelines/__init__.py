0db572e5479b8ede0b3e34c9e1ed81cc6ee7cda6
Achilles Rasquinha <achillesrasquinha@gmail.com>
2022-05-26 13:29:58 -0500

    Initial Commit

---
+++ src/gempy/pipelines/__init__.py
@@ -0,0 +1,1 @@
+from gempy.pipelines.train import train
---
+++ src/gempy/pipelines/train.py
@@ -0,0 +1,11 @@
+from gempy.__attr__ import __name__ as NAME
+from gempy.data import get_data_dir
+
+def build_model():
+    pass
+    # do something ...
+
+def train(data_dir = None, artifacts_dir = None, *args, **kwargs):
+    data_dir = get_data_dir(NAME, data_dir)
+    model    = build_model()
+    # do something ...
