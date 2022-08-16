dbdb4b32b525f3442e31e83146e56d467fd0442e
Achilles Rasquinha <achillesrasquinha@Achilless-MacBook-Air-2.local>
2022-08-16 12:38:28 -0500

    feat: fix gan

--- src/gempy/pipelines/train.py
+++ src/gempy/pipelines/train.py
@@ -1,13 +1,25 @@
 from gempy.__attr__ import __name__ as NAME
 from gempy.data import get_data_dir
+from gempy import settings, dops
 
-def build_model():
+def build_model(artifacts_path = None):
+    dropout_rate  = settings.get("dropout_rate")
+    batch_norm    = settings.get("batch_norm")
+
     from deeply.model.gan import GAN
 
-    model = GAN() 
+    model = GAN()
     # do something ...
+    pass
 
-def train(data_dir = None, artifacts_dir = None, *args, **kwargs):
+def train(data_dir = None, artifacts_dir = None, check = False, *args, **kwargs):
+    batch_size    = 1 if check else settings.get("batch_size")
+    learning_rate = settings.get("learning_rate")
+    epochs        = 1 if check else settings.get("epochs")
+
     data_dir = get_data_dir(NAME, data_dir)
     model    = build_model()
+    
+    dops.watch(model)
+
     # do something ...
